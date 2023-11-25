a221e10cf248919786b91a1b90b1ccb7371b0ae1
xulei <xuyalei0408@gmail.com>
2023-11-25 14:31:40 +0800

    modified:   mmseg/models/uda/__init__.py

--- mmseg/models/uda/__init__.py
+++ mmseg/models/uda/__init__.py
@@ -5,6 +5,6 @@
 
 from mmseg.models.uda.dacs import DACS
 from mmseg.models.uda.vecr import VECR
-from DAFormer.mmseg.models.uda.vecr_prog import VECR_ProG
+from mmseg.models.uda.vecr_prog import VECR_ProG
 
 __all__ = ['DACS', 'VECR', 'VECR_ProG']
